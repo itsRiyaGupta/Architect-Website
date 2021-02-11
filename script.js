e9ec597e194dadd84ea0d78430ecfc1b5e63e78c
itsRiyaGupta <68533998+itsRiyaGupta@users.noreply.github.com>
2021-02-11 14:33:10 +0530

    Add files via upload
    Made this website as a template for clients

---
+++ script.js
@@ -0,0 +1,17 @@
+document
+  .querySelector(".hamburger-menu")
+  .addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle("change");
+  });
+
+// const button = document.querySelector(".hamburger-menu");
+// const sidebar = document.querySelector(".sidebar");
+
+// button.addEventListener("click", function () {
+//   sidebar.classList.toggle("change");
+// });
+
+// const display = document.querySelector(".hamburger-menu");
+// display.addEventListener("click", function () {
+//   button.classList.toggle("hamburger-menu-dissapear");
+// });
